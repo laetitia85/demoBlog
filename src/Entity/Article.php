dcd432982f71069b5508a13423f8a544369ddbbb
Laetitia <titiluna.sky85@gmail.com>
2022-02-11 11:23:24 +0100

    update: formulaire modification de l'article + contraintes sur les champs du formulaire

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ArticleRepository::class)]
 class Article
@@ -14,12 +15,22 @@ class Article
     private $id;
 
     #[ORM\Column(type: 'string', length: 255)]
+    // on précise le nombre de caractères min et max que le titre peut contenir
+    #[Assert\Length(min:5, max:255, minMessage:"Le titre doit comporter au moins {{ limit }} caractères.")]
+    // on précise avec la contrainte NotNull() qu'on ne veut pas que le champ soit null et on ajoute un msg en paramètre
+    #[Assert\NotNull(message:"Le champ titre ne peut pas être vide! Veuillez insérer un titre")]
     private $title;
 
     #[ORM\Column(type: 'text')]
+    // on précise le nombre de caractères min que le contenu peut contenir
+    #[Assert\Length(min:10, minMessage:"Le contenu doit comporter au moins {{ limit }} caractères.")]
+    // on précise avec la contrainte NotNull() qu'on ne veut pas que le champ soit null et on ajoute un msg en paramètre
+    #[Assert\NotNull(message:"Le champ contenu ne peut pas être vide! Veuillez insérer un contenu")]
     private $content;
 
     #[ORM\Column(type: 'string', length: 255)]
+    // on précise avec la contrainte NotNull() qu'on ne veut pas que le champ soit null et on ajoute un msg en paramètre
+    #[Assert\NotNull(message:"Le champ image ne peut pas être vide! Veuillez insérer l'url de l'image")]
     private $image;
 
     #[ORM\Column(type: 'datetime')]
